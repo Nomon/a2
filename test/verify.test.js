6f8f122e51dbb025ffd73f87186dc378679c6c16
Matti Savolainen <matti@applifier.com>
2012-04-09 14:38:40 +0300

    tests for uri and redsponse_type verification

---
+++ test/verify.test.js
@@ -0,0 +1,22 @@
+
+suite('OAUTH2 Server query parameter verifier', function() {
+  test('only code and token response_type allowed', function() {
+    assert(oauth2.verify.responseType("code"));
+    assert(oauth2.verify.responseType("token"));
+    assert(!oauth2.verify.responseType("kekkonen"));
+  });
+  test('redirect_uri must match', function() {
+    var client = {
+      redirect_uri: ["http://kissa.com/oauth/callback","http://kissa.com/oauth2/callback"]
+    };
+    var redirect_uri = "http://kissa.com/oauth/callback";
+    assert(oauth2.verify.redirectUri(client,redirect_uri));
+    assert(oauth2.verify.redirectUri(client));
+    redirect_uri = "http://nonexisting.com/";
+    assert(!oauth2.verify.redirectUri(client, redirect_uri));
+    redirect_uri = "http://kissa.com/oauth2/callback";
+    assert(oauth2.verify.redirectUri(client, redirect_uri));
+    client.redirect_uri = "http://kissa.com/oauth2/callback";
+    assert(oauth2.verify.redirectUri(client, redirect_uri));
+  });
+});
